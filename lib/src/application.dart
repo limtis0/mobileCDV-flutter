98ca0fd8b10c7bec57295490db14b2e86fabe054
juzikaster <izax4321@gmail.com>
2022-03-02 21:15:56 +0100

    refactor

---
+++ lib/src/application.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MainApp extends StatelessWidget {
+  const MainApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome to Flutter'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
