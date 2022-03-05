043dea329c2e41a110f202d294630ed836d972da
Vladimir <limtis2109@gmail.com>
2022-03-05 23:38:06 +0100

    Added dependencies for working with files. Created files.dart

---
+++ lib/src/files.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+
+Future<String> get _localPath async
+{
+   final directory = await getApplicationDocumentsDirectory();
+   return directory.path;
+}
+
+// TODO Доделать...
+
