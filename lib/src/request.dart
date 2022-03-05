570f5d905ca371ef7d2c4e6f4a8a66e7e21a5b6d
Vladimir <limtis2109@gmail.com>
2022-03-05 15:10:43 +0100

    Schedule request

--- lib/src/request.dart
+++ lib/src/request.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
+// LOGIN
 class LoginResponse
 {
   final bool result;
@@ -45,5 +46,90 @@ Future<LoginResponse> fetchLogin(String login, String password) async
   else
   {
     throw Exception('Failed to login.');
+  }
+}
+
+// SCHEDULE
+class ScheduleTableItem
+{
+  final int id;
+  final String status;
+  final String subject;
+  final String subjectName;
+  final String groupNumber;
+  final String startDate;
+  final String endDate;
+  final String form;
+  final String teacher;
+  final String room;
+  final String meetLink;
+  final String surveyStatus;
+
+  const ScheduleTableItem({
+    required this.id,
+    required this.status,
+    required this.subject,
+    required this.subjectName,
+    required this.groupNumber,
+    required this.startDate,
+    required this.endDate,
+    required this.form,
+    required this.teacher,
+    required this.room,
+    required this.meetLink,
+    required this.surveyStatus,
+  });
+
+  // TODO Check whether factory is ruining it
+  factory ScheduleTableItem.fromJson(Map<String, dynamic> json)
+  {
+    return ScheduleTableItem
+    (
+      id: json['Id'],
+      status: json['Status'],
+      subject: json['Subject'],
+      subjectName: json['ThemaGroup'],
+      groupNumber: json['GroupNr'],
+      startDate: json['Start'],
+      endDate: json['End'],
+      form: json['Form'],
+      teacher: json['Teacher'],
+      room: json['Room'],
+      meetLink: json['HangoutLink'],
+      surveyStatus: json['SurveyStatus'],
+    );
+  }
+
+  // TODO Delete after testing
+  @override
+  String toString()
+  {
+    return '$id, $subjectName';
+  }
+}
+
+Future<List> fetchSchedule(String studentId, String dateFrom, String dateTo, String token) async
+{
+  String url = 'https://api.cdv.pl/mobilnecdv-api/schedule/student/$studentId/1/$dateFrom/$dateTo';
+  final response = await http.get(
+    Uri.parse(url),
+    headers: <String, String>
+    {
+      'Authorization': 'Bearer $token',
+    }
+  );
+
+  if (response.statusCode == 200)
+  {
+    List schedule = [];
+    for (var lesson in jsonDecode(response.body))
+    {
+      schedule.add(ScheduleTableItem.fromJson(lesson));
+    }
+    return schedule;
+  }
+  else
+  {
+    throw Exception('Failed to get schedule.');
   }
 }
