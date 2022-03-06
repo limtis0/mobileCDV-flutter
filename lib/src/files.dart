669fa58b9e52e1e468454914bff14706eb7fdd97
Vladimir <limtis2109@gmail.com>
2022-03-06 14:16:33 +0100

    Added Image saving

--- lib/src/files.dart
+++ lib/src/files.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:typed_data';
 import 'package:path_provider/path_provider.dart';
 
 Future<String> get _localPath async
@@ -7,8 +8,14 @@ Future<String> get _localPath async
    return directory.path;
 }
 
-// void saveImage(String fileName) async
-// {
-//    String path = await _localPath;
-// }
+// Gets Uint8List on input and saves it as an image (i.e. test.png)
+void saveImage(Uint8List imageBytes, String fileName) async
+{
+   final String filePath = await _localPath;
+   final file = File('$filePath/$fileName');
+   file.writeAsBytes(imageBytes);
+}
 
+/* TODO Choose the library with Image data type that suits front-end
+   and make getImageFromFile() function;
+ */
