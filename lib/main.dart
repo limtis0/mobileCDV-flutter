5bc0bfdb1068d06e261a9010301aea0772bf8523
juzikaster <izax4321@gmail.com>
2022-03-06 15:47:53 +0100

    Localization

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:intl/date_symbol_data_local.dart';
 import 'src/lib/lozalization/localization_manager.dart';
 
 void main() {
+  initLocalization("en");
   initializeDateFormatting().then((_) => runApp(const MainApp()));
 }
 
--- lib/src/lib/lozalization/localization_manager.dart
+++ lib/src/lib/lozalization/localization_manager.dart
@@ -3,29 +3,32 @@ import 'dart:io';
 
 import 'package:csv/csv.dart';
 
-/*
-*
-* fields[0] - Columns names
-*
-*/
-
 List? fields;
-int? _localeid;
+int? localeId;
 
 void initLocalization([String locale = "pl"]) async
 {
+  switch(locale){
+    case "en":
+      localeId = 1;
+      break;
+    case "pl":
+      localeId = 2;
+      break;
+    default:
+      localeId = 2;
+      break;
+  }
   final input = File('file.csv').openRead();
   fields = await input.transform(utf8.decoder).transform(const CsvToListConverter()).toList();
 }
 
-
-
 String? getTextFromKey([String key = "noKey"])
 {
-  if(_localeid != null) {
+  if(localeId != null) {
     for (int i = 0; i != fields?.length; i++) {
       if (fields?[i][0] == key) {
-        return fields?[i][_localeid];
+        return fields?[i][localeId];
       }
     }
   }
