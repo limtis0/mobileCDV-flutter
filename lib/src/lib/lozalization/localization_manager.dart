7a81f803a0a4c29ead8dbe97c3b429e2e13f2900
juzikaster <izax4321@gmail.com>
2022-03-06 15:38:53 +0100

    localization

--- lib/src/lib/lozalization/localization_manager.dart
+++ lib/src/lib/lozalization/localization_manager.dart
@@ -1,16 +1,33 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:csv/csv.dart';
 
-final input = File('file.csv').openRead();
+/*
+*
+* fields[0] - Columns names
+*
+*/
 
-void initLocalization([String locale = "pl"])
+List? fields;
+int? _localeid;
+
+void initLocalization([String locale = "pl"]) async
 {
-
+  final input = File('file.csv').openRead();
+  fields = await input.transform(utf8.decoder).transform(const CsvToListConverter()).toList();
 }
 
-String getTextFromKey([String key = "noKey"])
-{
 
-  return "S";
+
+String? getTextFromKey([String key = "noKey"])
+{
+  if(_localeid != null) {
+    for (int i = 0; i != fields?.length; i++) {
+      if (fields?[i][0] == key) {
+        return fields?[i][_localeid];
+      }
+    }
+  }
+  return "Translation missing, key: $key";
 }
