5a9e0f41648511318cc7eccd2365268be5f0a93b
Vladimir <limtis2109@gmail.com>
2022-04-28 14:58:34 +0200

    Cleaned main

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:mobile_cdv/src/logic/main_activity.dart';
 import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-
 import 'package:mobile_cdv/src/lib/localization/localization_manager.dart';
 import 'package:mobile_cdv/src/logic/notifications.dart';
 import 'package:mobile_cdv/src/logic/storage/shared_prefs.dart';
@@ -18,13 +16,12 @@ Future<void> startApp() async
 {
   WidgetsFlutterBinding.ensureInitialized(); // fix error with async function
 
-  final SharedPreferences prefs = await SharedPreferences.getInstance();
   await initPrefs();
   await globals.loadFromPrefs();
 
   NotificationService().init(); // init notification service
 
-  await initLocalization(prefs.getString('localization') ?? 'pl'); // init localization
+  await initLocalization(globals.locale); // init localization
 
   showTimetable();
 }
@@ -32,14 +29,14 @@ Future<void> startApp() async
 
 Future<void> showTimetable() async
 {
-  final SharedPreferences prefs = await SharedPreferences.getInstance();
-  try {
-    await activitySignIn(prefs.getString('savedEmail')!, prefs.getString('savedPassword')!, false);
+  if (globals.isLoggedIn) {
+    try {
+    await activitySignIn(globals.email, globals.pass, false);
+    }
+    catch(e){
+    await activitySignOut();
+    }
   }
-  catch(e){
-    globals.isLoggedIn = false;
-  }
-
   initializeDateFormatting().then((_) async => runApp(
       ChangeNotifierProvider<ThemeModel>(
         create: (BuildContext context) => ThemeModel(),
